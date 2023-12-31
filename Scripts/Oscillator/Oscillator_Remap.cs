a692f48bcdbc6a922d9c839e606f90aa2ed341ed
dlobser <davidlobser@yahoo.com>
2023-12-31 18:49:11 -0500

    remap fix

--- Scripts/Oscillator/Oscillator_Remap.cs
+++ Scripts/Oscillator/Oscillator_Remap.cs
@@ -17,14 +17,14 @@ namespace ON.synth
             float inLow = Mathf.Max(oscillator.clampLow,oscillator.trough*oscillator.multiply+oscillator.offset);
             float inHigh = Mathf.Min(oscillator.clampHigh,oscillator.crest*oscillator.multiply+oscillator.offset);
             value = ON.synth.Synth_Util.map(oscillator.GetValue(), inLow, inHigh, lowValue, highValue);
-            return value;
+            return lowValue==highValue ? lowValue : value;
         }
 
         public override float GetValue(float t)
         {
             float inLow = Mathf.Max(oscillator.clampLow,oscillator.trough*oscillator.multiply+oscillator.offset);
             float inHigh = Mathf.Min(oscillator.clampHigh,oscillator.crest*oscillator.multiply+oscillator.offset);
-            return ON.synth.Synth_Util.map(oscillator.GetValue(t), inLow, inHigh, lowValue, highValue);
+            return lowValue==highValue ? lowValue : ON.synth.Synth_Util.map(oscillator.GetValue(t), inLow, inHigh, lowValue, highValue);
         }
     }
 }
